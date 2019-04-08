fe9aae07c1a00f8207b64e4d65fcecf4cfd3037b
Felivian <mateusz.hekert@gmail.com>
2019-04-08 21:38:01 +0200

    nodes extraction redesign

---
+++ client/src/components/PostParagraphBuilder.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class PostParagraphBuilder extends React.Component {
+	render() {
+		return null;
+	}
+}
+
+export default PostParagraphBuilder;
--- client/src/components/PostViewer.js
+++ client/src/components/PostViewer.js
@@ -1,40 +1,67 @@
 import React, { Component } from 'react';
+import parse5 from 'parse5';
 
 class PostViewer extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 
-	populateContentBytags(tag, content) {
-		if (content.indexOf(tag.start) !== -1) {
-			let contentAfterRemoving =
-				content.slice(0, content.indexOf(tag.start)) +
-				content.slice(content.indexOf(tag.end) + tag.end.length, content.length);
-			let contentOfTag = content.slice(content.indexOf(tag.start) + tag.start.length, content.indexOf(tag.end));
+	// populateContentBytags(tag, content) {
+	// 	if (content.indexOf(tag.start) !== -1) {
+	// 		let contentAfterRemoving =
+	// 			content.slice(0, content.indexOf(tag.start)) +
+	// 			content.slice(content.indexOf(tag.end) + tag.end.length, content.length);
+	// 		let contentOfTag = content.slice(content.indexOf(tag.start) + tag.start.length, content.indexOf(tag.end));
 
-			return this.populateContentBytags(tag, contentAfterRemoving) + '!@#$' + contentOfTag;
+	// 		return this.populateContentBytags(tag, contentAfterRemoving) + '!@#$' + contentOfTag;
+	// 	} else {
+	// 		return '!@#$';
+	// 	}
+	// }
+
+	// decodePostContent(content, tags, ignoredArr) {
+	// 	let obj = {};
+	// 	tags.forEach((tag) => {
+	// 		let contentByTagArr = this.populateContentBytags(tag, content).split('!@#$').filter((el) => el != '');
+	// 		contentByTagArr = contentByTagArr.filter((el) => !ignoredArr.some((ignoredVal) => ignoredVal === el));
+	// 		obj[tag.name] = contentByTagArr;
+	// 	});
+	// 	return obj;
+	// }
+
+	getNodes(tag, node) {
+		const childNodes = node.childNodes;
+		let arr = [];
+		if (childNodes !== undefined) {
+			childNodes.forEach((element) => {
+				if (element.nodeName === tag) {
+					arr = arr.concat(element, this.getNodes(tag, element));
+				} else {
+					arr = arr.concat(this.getNodes(tag, element));
+				}
+			});
+			return arr;
 		} else {
-			return '!@#$';
+			if (node.nodeName === tag) {
+				return node;
+			} else {
+				return [];
+			}
 		}
-	}
-
-	decodePostContent(content) {
-		let tags = [ { name: 'p', start: '<p>', end: '</p>' }, { name: 'table', start: '<table>', end: '</table>' } ];
-		let obj = {};
-		tags.forEach((tag) => {
-			obj[tag.name] = this.populateContentBytags(tag, content).split('!@#$').filter((el) => el != '');
-		});
-		return obj;
 	}
 
 	render() {
 		const postId = this.props.post ? this.props.post.id : '';
-		const postContent = this.props.post ? this.props.post.content.rendered : '{}';
-		const decodedContent = this.decodePostContent(postContent);
+		const postContent = this.props.post ? this.props.post.content.rendered : '';
+		// const tags = [ { name: 'p', start: '<p>', end: '</p>' }, { name: 'table', start: '<table>', end: '</table>' } ];
+		// const decodedContent = this.decodePostContent(postContent, tags, [ '', '&nbsp;' ]);
+
+		let arr2 = this.getNodes('p', parse5.parseFragment(postContent));
+		console.log(arr2);
 		return (
 			<React.Fragment>
 				<h2>{postId}</h2>
-				<p>{JSON.stringify(decodedContent)}</p>
+				<p>{JSON.stringify(postContent)}</p>
 			</React.Fragment>
 		);
 	}
